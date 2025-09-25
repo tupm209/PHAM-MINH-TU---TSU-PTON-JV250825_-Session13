cfa70da304e6b135f77a0528fc6af272f90e40bc
tupm209 <tu.pm209@gmail.com>
2025-09-25 23:21:52 +0900

    Add files via upload

---
+++ hw01/hw01.js
@@ -0,0 +1,10 @@
+let arr = ["Táo", "Lê", "Đào"];
+
+arr.push("Dưa hấu", "Dưa chuột");
+// console.log(arr);
+
+arr.unshift("Chuối", "Mận");
+// console.log(arr);
+
+arr.splice(3, 0, "Cam", "Quýt", "Hồng");
+console.log(arr);
